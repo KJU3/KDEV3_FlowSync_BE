760c33ff987deb22b0967b75497a5da881087e30
KJU3 <kju_3@naver.com>
2025-01-12 23:04:10 +0900

    feat: cicd test4

--- module-api/src/main/java/com/checkping/api/ApiApplication.java
+++ module-api/src/main/java/com/checkping/api/ApiApplication.java
@@ -3,6 +3,7 @@ package com.checkping.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -13,7 +14,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
                 "com.checkping.service",
                 "com.checkping.infra",
                 "com.checkping.domain",
-        }
+        },
+        exclude = {SecurityAutoConfiguration.class}
 )
 @EntityScan("com.checkping.domain")
 @EnableJpaRepositories(basePackages = "com.checkping.infra.repository")
