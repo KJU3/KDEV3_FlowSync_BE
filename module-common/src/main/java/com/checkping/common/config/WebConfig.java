7021cf619df1e289677abde65eeaae562510efbf
KJU3 <kju_3@naver.com>
2025-01-13 11:17:58 +0900

    feat: cicd test7

--- module-common/src/main/java/com/checkping/common/config/WebConfig.java
+++ module-common/src/main/java/com/checkping/common/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080/", "http://15.164.86.69:8080/")
+                .allowedOrigins("http://localhost:8080", "http://15.164.86.69:8080", "http://15.164.86.69")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 . allowedHeaders("*")
                 .allowCredentials(true)
